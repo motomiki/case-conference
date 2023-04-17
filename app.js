e80afad9d6d3eb4ba1f902d29465e53d06e37cbc
Kimitomo TADANO <62192363+motomiki@users.noreply.github.com>
2023-04-17 16:15:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const { Configuration, OpenAIApi } = require("openai");
+const http = require('http');
+const socketIO = require("socket.io");
 const app = express();
 
 // Express settings
@@ -16,43 +18,52 @@ const openai = new OpenAIApi(configuration);
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.post('/api/query', async (req, res) => {
-  const prompt = req.body.prompt;
+const server = http.createServer(app);
+const io = socketIO(server);
 
-  try {
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: `
-      # 命令書：
-      あなたは有能なファシリテーターです。「${prompt}」に対処するための最善策を、以下の専門家たちと協力して提案してください。
+io.on("connection", (socket) => {
+  console.log("User connected");
 
-      # 待機者：
-      ・ 児童相談員
-      ・ 公認心理師
-      ・ 社会福祉士
+    // クエリを処理するためのイベントリスナー
+    socket.on('query', async (queryData) => {
+      const prompt = queryData.prompt;
 
-      # 手順：
-      1. それぞれの専門家が、自分の視点から相談を分析し、提案を出します。
-      2. 提案を議論し、統合されたアプローチを形成します。
-      3. 最善策をエグゼクティブサマリー形式でまとめます。
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: `
+        # 命令書：
+        あなたは有能なファシリテーターです。「${prompt}」に対処するための最善策を、以下の専門家たちと協力して提案してください。
 
-      # 制約：
-      ・ 出力は「エグゼクティブサマリー形式」とする
-      ・ 最初に「参加者」を箇条書き、続いて「概要」を記載する
-      ・ 「結論」を50文字以内の文章でまとめる
-      ・ 「要点」を箇条書きで書き出す
-      ・ 「ブレークダウンの内容と結論」を80文字以内で書き出す
-      ・ 「その他参考情報」を60字以内の文章でまとめ、ユニークな議論を盛り込むこと
-      `}],
-    });
-    res.json(completion.data.choices[0].message);
-  } catch (error) {
-    console.error(error.response.data);
-    res.status(500).json({ error: 'Error calling OpenAI API.', details: error.response.data });
-  }
+        # 待機者：
+        ・ 児童相談員
+        ・ 公認心理師
+        ・ 社会福祉士
+
+        # 手順：
+        1. それぞれの専門家が、自分の視点から相談を分析し、提案を出します。
+        2. 提案を議論し、統合されたアプローチを形成します。
+        3. 最善策をエグゼクティブサマリー形式でまとめます。
+
+        # 制約：
+        ・ 出力は「エグゼクティブサマリー形式」とする
+        ・ 最初に「参加者」を箇条書き、続いて「概要」を記載する
+        ・ 「結論」を50文字以内の文章でまとめる
+        ・ 「要点」を箇条書きで書き出す
+        ・ 「ブレークダウンの内容と結論」を80文字以内で書き出す
+        ・ 「その他参考情報」を60字以内の文章でまとめ、ユニークな議論を盛り込むこと
+        `}],
+      });
+      io.emit('message', completion.data.choices[0].message);
+    } catch (error) {
+      socket.on('disconnect', () => {
+        console.log('User disconnected');
+      });
+    }
+  });
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Application listening at ${port}`);
 });
